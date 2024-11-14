bb0d1c6ee3a75677af65934ed9f2b9f21ec56508
fastestsunil <sunil141292@gmail.com>
2024-11-14 16:31:13 +0530

    sitemap and robotss txt

---
+++ src/lib/sitemapUtils.js
@@ -0,0 +1,28 @@
+const servicesData = [
+  {
+    slug: 'digital-marketing-services',
+  },
+  {
+    slug: 'social-media-marketing-services',
+  },
+  {
+    slug: 'seo-services',
+  },
+  {
+    slug: 'influencer-marketing-services',
+  },
+  {
+    slug: 'web-development-services',
+  },
+  {
+    slug: 'pr-agencys',
+  },
+  {
+    slug: 'media-buying-company',
+  },
+  {
+    slug: 'search-engine-marketing-services',
+  },
+];
+
+module.exports = servicesData;
